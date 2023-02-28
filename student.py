0a3fcd7358f8764254825f3b2b4f1c46a6777da3
Андрей Боев <a.boev.it@gmail.com>
2023-02-28 14:07:05 +0300

    Initial commit

---
+++ student.py
@@ -0,0 +1,13 @@
+class Student:
+    # Конструктор
+    def __init__(self, first_name, last_name, middle_name, photo, birth_date, phone, address, group, average_mark):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.middle_name = middle_name
+        self.photo = photo
+        self.birth_date = birth_date
+        self.phone = phone
+        self.address = address
+        self.group = group
+        self.average_mark = average_mark
+    
